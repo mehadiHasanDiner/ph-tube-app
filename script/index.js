213d14fb82b11f78683fdbc1c8e81c326e3310a6
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2026-02-17 18:25:17 +0600

    button done

--- script/index.js
+++ script/index.js
@@ -1,1 +1,18 @@
-console.log("first");
+function loadCategoryBtn() {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategoryBtn(data.categories));
+}
+
+const displayCategoryBtn = (categories) => {
+  const categoryContainerDiv = document.getElementById("category-container");
+  console.log(categories);
+  categories.forEach((cate) => {
+    console.log(cate.category);
+    categoryContainerDiv.innerHTML = `
+    <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">All</button>
+    `;
+  });
+};
+
+loadCategoryBtn();
