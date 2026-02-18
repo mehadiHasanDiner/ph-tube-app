4e9d99174516f12e850fac188afdfa989a3aaf93
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2026-02-18 10:50:14 +0600

    loading video api done

--- script/index.js
+++ script/index.js
@@ -1,18 +1,33 @@
+// loading category button
 function loadCategoryBtn() {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res) => res.json())
     .then((data) => displayCategoryBtn(data.categories));
 }
 
+// displaying category button
 const displayCategoryBtn = (categories) => {
   const categoryContainerDiv = document.getElementById("category-container");
-  console.log(categories);
   categories.forEach((cate) => {
-    console.log(cate.category);
-    categoryContainerDiv.innerHTML = `
-    <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">All</button>
+    const buttonDiv = document.createElement("div");
+    buttonDiv.innerHTML = `
+        <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cate.category}</button>
     `;
+    categoryContainerDiv.append(buttonDiv);
   });
+};
+
+// loading videos
+function loadVideos() {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos));
+}
+
+// displaying videos
+const displayVideos = (videos) => {
+  console.log(videos);
+  const displayVideosDiv = document.getElementById("display-videos");
 };
 
 loadCategoryBtn();
