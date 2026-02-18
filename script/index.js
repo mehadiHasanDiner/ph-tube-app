3cfba9d24fbe910dd645e5badab3b825848d6736
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2026-02-18 17:48:55 +0600

    diplaying done running

--- script/index.js
+++ script/index.js
@@ -7,13 +7,13 @@ function loadCategoryBtn() {
 
 // displaying category button
 const displayCategoryBtn = (categories) => {
-  const categoryContainerDiv = document.getElementById("category-container");
+  const categoryContainer = document.getElementById("category-container");
   categories.forEach((cate) => {
     const buttonDiv = document.createElement("div");
     buttonDiv.innerHTML = `
         <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cate.category}</button>
     `;
-    categoryContainerDiv.append(buttonDiv);
+    categoryContainer.append(buttonDiv);
   });
 };
 
@@ -27,7 +27,26 @@ function loadVideos() {
 // displaying videos
 const displayVideos = (videos) => {
   console.log(videos);
-  const displayVideosDiv = document.getElementById("display-videos");
+  const displayVideosContainer = document.getElementById("display-videos");
+  videos.forEach((video) => {
+    const displayVideoDiv = document.createElement("div");
+    displayVideoDiv.innerHTML = `
+    <div className="card bg-base-100 shadow-sm">
+            <figure>
+                <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">Card Title</h2>
+                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
+    `;
+    displayVideosContainer.append(displayVideoDiv);
+  });
 };
 
 loadCategoryBtn();
+loadVideos();
