3461885146f02ed7e83c981de639c24b9ac74aae
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2026-02-19 06:36:20 +0600

    card section running

--- script/index.js
+++ script/index.js
@@ -31,15 +31,15 @@ const displayVideos = (videos) => {
   videos.forEach((video) => {
     const displayVideoDiv = document.createElement("div");
     displayVideoDiv.innerHTML = `
-    <div className="card bg-base-100 shadow-sm">
+    <div class="card bg-base-100 shadow-sm">
             <figure>
-                <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+                <img src="${video.thumbnail}" alt="Shoes" />
             </figure>
-            <div className="card-body">
-                <h2 className="card-title">Card Title</h2>
+            <div class="card-body">
+                <h2 class="card-title">Card Title</h2>
                 <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
                 </div>
             </div>
         </div>
